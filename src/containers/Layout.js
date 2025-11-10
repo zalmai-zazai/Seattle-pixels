763cb3cdf7f5523d483355963b29e71e76605604
zalmai-zazai <shahzalmai98@gmail.com>
2025-11-10 15:41:48 -0800

    solved 2

--- src/containers/Layout.js
+++ src/containers/Layout.js
@@ -1,31 +1,8 @@
-import Footer from "./Footer";
-import Navbar from "./Navbar";
-import Link from "next/link";
-import Navlinks from "./Navlinks";
 import Head from "next/head";
-import ModalLayout from "./ModalLayout";
-import { useDispatch, useSelector } from "react-redux";
-import { setLoggedIn, setToken } from "@/store/userSlice";
-import { useEffect } from "react";
-import LeftSidebar from "./LeftSidebar";
-import NavProfileLinks from "./NavProfileLinks";
+import Navbar from "./Navbar";
+import Footer from "./Footer";
 
-export default function Layout({ children }) {
-  const logoutUser = () => {};
-
-  const { isLoggedIn, credits, scrollId } = useSelector((state) => state.user);
-  const dispatch = useDispatch();
-
-  useEffect(() => {
-    //initAPIS and Auth
-    const TOKEN = localStorage.getItem("token");
-    if (TOKEN) {
-      dispatch(setLoggedIn(true));
-      dispatch(setToken(TOKEN));
-      // getCurrentCredits()
-    }
-  }, []);
-
+export default function CleanLayout({ children }) {
   return (
     <>
       <Head>
@@ -59,40 +36,11 @@ export default function Layout({ children }) {
         ></script> */}
         <meta itemProp="image" content="/logo.png" />
       </Head>
-      <div className="drawer">
-        <input id="my-drawer-3" type="checkbox" className="drawer-toggle" />
-        <div className="drawer-content flex flex-col">
-          <div className="overflow-y-auto  flex flex-col ">
-            <Navbar />
 
-            <div className="overflow-y-auto">
-              <main>{children}</main>
-              <Footer />
-            </div>
-          </div>
-        </div>
-
-        <div className="drawer-side ">
-          <label htmlFor="my-drawer-3" className="drawer-overlay"></label>
-          <ul className="menu p-4 w-80 h-full bg-base-100">
-            <li className="mr-2 font-bold text-gray-500">
-              <Link href="/">
-                {" "}
-                <span className="material-symbols-outlined  text-blue-300">
-                  home
-                </span>{" "}
-                Home
-              </Link>
-            </li>
-            <Navlinks />
-            <div className="mt-12"></div>
-            <NavProfileLinks />
-          </ul>
-        </div>
-      </div>
-
-      <LeftSidebar />
-      <ModalLayout />
+      {/* Super simple layout - no scroll containers */}
+      <Navbar />
+      <div className="min-h-screen">{children}</div>
+      <Footer />
     </>
   );
 }
